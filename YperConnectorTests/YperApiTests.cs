ac21670adeb4972da51b8c917ca74b59e869a1b4
Alexandre Dubois <contact@alexandredubois.com>
2021-09-12 09:28:01 +0200

    Add first unit test to validate authentication scheme

--- YperConnectorTests/YperApiTests.cs
+++ YperConnectorTests/YperApiTests.cs
@@ -1,13 +1,28 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YperConnector;
 
 namespace YperConnectorTests
 {
     [TestClass]
-    public class UnitTest1
+    public class YperApiTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private IConfiguration _config;
+
+        public YperApiTests()
         {
+            _config = new ConfigurationBuilder()
+                .AddJsonFile("UnitTestConfig.json")
+                .Build();
+        }
+
+        [TestMethod]
+        public void RetrieveBearerToken()
+        {
+            var clientId = _config.GetSection("credentials")["appId"];
+            var clientSecret = _config.GetSection("credentials")["appSecret"];
+            var environment = "production".Equals(_config.GetSection("credentials")["environment"]) ? Environment.Production : Environment.Sandbox;
+            var apiInstance = YperApi.Initialize(environment, clientId, clientSecret);
         }
     }
 }
